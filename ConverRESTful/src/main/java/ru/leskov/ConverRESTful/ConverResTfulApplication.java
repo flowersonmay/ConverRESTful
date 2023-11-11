090384b876762911b9463d87578be4bf9d5e3149
leskov <flowersonmay@gmail.com>
2023-11-11 19:59:19 +0300

    Initial commit

---
+++ ConverRESTful/src/main/java/ru/leskov/ConverRESTful/ConverResTfulApplication.java
@@ -0,0 +1,13 @@
+package ru.leskov.ConverRESTful;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConverResTfulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConverResTfulApplication.class, args);
+	}
+
+}
