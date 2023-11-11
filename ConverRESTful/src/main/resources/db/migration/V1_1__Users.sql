ea656f0775f654f41718cca2276e5d6cb4e98a52
leskov <flowersonmay@gmail.com>
2023-11-12 01:09:33 +0300

    Добавлен flyway

---
+++ ConverRESTful/src/main/resources/db/migration/V1_1__Users.sql
@@ -0,0 +1,5 @@
+insert into users(username, password)
+values ('batman', '$2a$10$SGLmIEiyo14DEhsVYdyWAujryc98pesXn9aaEGd8XqN4ypo1U4nOy'),
+       ('admin', '$2a$10$qfIklUEKKrEeDahSs3O6YOPrjkT1f8XoQkcHCV/k8XMMdgpFG06vW');
+-- Пароль от batman = test
+-- Пароль от admin = admin
